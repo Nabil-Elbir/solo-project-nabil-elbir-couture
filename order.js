3652e9aa05b72f8cb316ba99f5baf9f49c39de46
Nabil-Elbir <nabil.elbir@gmail.com>
2020-12-15 19:34:20 +0100

    Final Push

---
+++ order.js
@@ -0,0 +1,15 @@
+$('#mainBtn1').click(function(){
+   window.location = 'gallery.html';
+});
+    
+$('#mainBtn2').click(function(){
+    window.location = 'order.html';
+});
+    
+$('#mainBtn3').click(function(){
+    window.location = 'index.html';
+});
+
+
+
+
